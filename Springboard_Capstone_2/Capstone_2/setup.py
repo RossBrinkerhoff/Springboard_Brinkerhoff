cd956a2a8d1e26a876a8fbc4d7137e42cf32fa46
RossBrinkerhoff <126492351+RossBrinkerhoff@users.noreply.github.com>
2023-05-15 08:19:23 -0400

    Commiting 01_rb_data_wrangling notebook and files

---
+++ Springboard_Capstone_2/Capstone_2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Springboard_Capstone_2',
+    author='Ross Brinkerhoff',
+    license='',
+)
